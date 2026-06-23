47b487bb09cca6a61eadf6cecc78d0988a8c0d8e
Polar <gui.oliveira.code@gmail.com>
2026-06-22 22:46:07 -0300

    🚧 Em progresso

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,61 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main () {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println (String.format ("Hello and welcome!"));
+import Model.Conta;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println ("i = " + i);
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    static void main () {
+        Scanner i = new Scanner (System.in);
+        boolean rodando = true;
+
+        ArrayList<Conta> listaContas = new ArrayList<> ();
+
+        while (rodando){
+            System.out.println ("*************************");
+            System.out.println ("     BANCO OPERAÇÕES     ");
+            System.out.println ("*************************");
+            System.out.println ("1 - Criar conta");
+            System.out.println ("2 - Listar todas as Contas");
+            System.out.println ("3 - Buscar conta por numero");
+            System.out.println ("4 - Atualizar dados da conta");
+            System.out.println ("5 - Apagar conta");
+            System.out.println ("6 - Sacar");
+            System.out.println ("7 - Depositar ");
+            System.out.println ("8 - Transferir valores entre contas ");
+            System.out.println ("9 - Sair ");
+            System.out.println ("*************************************");
+            System.out.println ("Entre com a opção desejada: ");
+            int opcao = i.nextInt ();
+
+
+            switch (opcao){
+                case 1:
+                    System.out.println ("Numero: ");
+                    int numero = i.nextInt ();
+
+                    System.out.println ("Agencia: ");
+                    int agencia = i.nextInt ();
+
+                    System.out.println ("Tipo da conta: ");
+                    String tipoDeConta = i.nextLine ();
+
+                    System.out.println ("Nome do Titular: ");
+
+
+
+
+
+
+                case 9:
+                    System.out.println ("Saindo do sistema...");
+                    rodando = false;
+                    break;
+
+                default:
+                    System.out.println ("Escolha uma opção valida");
+            }
+
+        }
+
     }
 }
---
+++ src/Model/Conta.java
@@ -0,0 +1,69 @@
+package Model;
+
+public class Conta {
+    // Atributos da classe
+    private int numeros;
+    private int agencia;
+    private String tipo;
+    private String titular;
+    private float saldo;
+
+    public Conta (int numeros, int agencia, String tipo, String titular, float saldo) {
+        this.numeros = numeros;
+        this.agencia = agencia;
+        this.tipo = tipo;
+        this.titular = titular;
+        this.saldo = saldo;
+    }
+
+    public void Imprimir(){
+        System.out.println ("DADOS DA CONTA");
+        System.out.println ("Número da conta" + this.numeros);
+        System.out.println ("Número da agencia" + this.agencia);
+        System.out.println ("Tipo de conta " +this.tipo);
+        System.out.println ("Titular: " +this.titular);
+        System.out.println ("Saldo R$:" +this.saldo);
+    }
+
+
+
+    public int getNumeros () {
+        return numeros;
+    }
+
+    public void setNumeros (int numeros) {
+        this.numeros = numeros;
+    }
+
+    public int getAgencia () {
+        return agencia;
+    }
+
+    public void setAgencia (int agencia) {
+        this.agencia = agencia;
+    }
+
+    public String getTipo () {
+        return tipo;
+    }
+
+    public void setTipo (String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTitular () {
+        return titular;
+    }
+
+    public void setTitular (String titular) {
+        this.titular = titular;
+    }
+
+    public float getSaldo () {
+        return saldo;
+    }
+
+    public void setSaldo (float saldo) {
+        this.saldo = saldo;
+    }
+}

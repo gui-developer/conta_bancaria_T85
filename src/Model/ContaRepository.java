c3bcdb0c9d423133879bcea009d9c5ca65563ced
Polar <gui.oliveira.code@gmail.com>
2026-06-23 23:39:06 -0300

    ✨ Adiciona interface ContaRepository

---
+++ src/Model/ContaRepository.java
@@ -0,0 +1,19 @@
+package Model;
+
+//Contrato
+public interface ContaRepository {
+
+    //CRUD
+    public void listarTodas();
+    public void cadastrar(Conta conta);
+    public void procurarPorNumero(int numero);
+    public void atualizar(Conta conta);
+    public void deletar(int numero);
+
+    //Métodos Bancários
+    public void sacar(int numero, float valor);
+    public void depositar(int numero, float valor);
+    public void transferir(int numeroOrigin, int numeroDestino, float valor);
+
+
+}
